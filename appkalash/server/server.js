6dadb13e53ae35d868183f0325e18de19aa769fc
Nithin-jain22 <nithinjain000.com>
2026-02-09 17:42:29 +0530

    fix: import supabase client

--- appkalash/server/server.js
+++ appkalash/server/server.js
@@ -8,6 +8,7 @@ import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 import { initSocket, emitToTeam } from "./socket.js";
+import { supabase } from "./supabase.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
